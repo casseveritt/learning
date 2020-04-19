366da48d61a0ff8f6cbefbb8c5e803444a5fbe8a
Lucas <lucas.everitt@gmail.com>
2020-04-19 17:14:31 -0500

    Triangular

--- gl.cpp
+++ gl.cpp
@@ -9,8 +9,26 @@
   You need dev packages to build and run this:
 
   sudo apt install libgles2-mesa-dev libglfw3-dev
-
 */
+
+
+static const char* vertex_shader_text =
+"#version 300 es\n"
+"in highp vec2 pos;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(pos, 0.0, 1.0);\n"
+"}\n";
+ 
+static const char* fragment_shader_text =
+"#version 300 es\n"
+"out mediump vec4 fragColor;\n"
+"void main()\n"
+"{\n"
+"    fragColor = vec4(1.0, 1.0, 0.0, 1.0);\n"
+"}\n";
+ 
+
 static void error_callback(int error, const char *description) {
   fprintf(stderr, "Error: %s\n", description);
 }
@@ -23,6 +41,7 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action,
 
 int main(void) {
   GLFWwindow *window;
+  GLuint vertex_shader, fragment_shader, program;
 
   glfwSetErrorCallback(error_callback);
 
@@ -45,6 +64,34 @@ int main(void) {
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1);
 
+  vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+  glCompileShader(vertex_shader);
+  fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+  glCompileShader(fragment_shader);
+
+  program = glCreateProgram();
+
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+
+  float vert2d[] = {
+    0.0f, 0.0f,
+    1.0f, 0.0f,
+    0.0f, 1.0f
+  };
+
+  GLuint vert_buffer;
+  glGenBuffers( 1, &vert_buffer);
+  glBindBuffer( GL_ARRAY_BUFFER, vert_buffer );
+  glBufferData( GL_ARRAY_BUFFER, sizeof( vert2d ), vert2d, GL_STATIC_DRAW);
+
+  GLint pos_loc = glGetAttribLocation(program, "pos");
+  glVertexAttribPointer( static_cast<GLuint>(pos_loc), 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), static_cast<void*>(0));
+  glEnableVertexAttribArray( static_cast<GLuint>(pos_loc) );
+
   int frame = 0;
   while (!glfwWindowShouldClose(window)) {
     int width, height;
@@ -52,8 +99,11 @@ int main(void) {
     glfwGetFramebufferSize(window, &width, &height);
 
     glViewport(0, 0, width, height);
-    glClearColor(0.01f, 20, (frame & 255) / 155.0f, 0);
+    glClearColor((frame & 255) * 255.0f, 20, (frame & 255) / 155.0f, 0);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(program);
+    glDrawArrays( GL_TRIANGLES, 0, 3);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
