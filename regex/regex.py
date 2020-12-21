b902a2c69e3aff9312f62f01d75ce982471183cb
Cass Everitt <cass@xyzw.us>
2020-12-21 14:36:53 -0600

    added regex code

--- regex/regex.py
+++ regex/regex.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+import re
 
 def main():
-    print("Hello, World!")
-    f = open("frankenstein.txt", "r")
+    f = open("gettysburg.txt", "r")
     for i in range(20):
         l = f.readline()
-        print(l, end='')
+        if l == "":
+            continue
+        words = re.findall(r'\b[A-Z][a-z]*\b', l)
+        print("line = {}".format(l))
+        for w in words:
+            print("w = {}".format(w))
+        space = 0
+        for j in l:
+            if j == ' ':
+                space += 1
+        print("spaces = {}, via re = {}".format(space, len(words)))
     f.close()
 
 if __name__ == "__main__":
