da1b19f5e8901e4ef50d6668c887a6ad3df405e5
Cass Everitt <cass@xyzw.us>
2020-12-21 15:36:09 -0600

    limit lines

--- regex/regex.py
+++ regex/regex.py
@@ -4,7 +4,11 @@ import re
 
 def main():
     with open("gettysburg.txt", "r") as f:
+        i = 0
         for l in f:
+            i += 1
+            if i >= 20:
+                break
             words = re.findall(r'\b[A-Za-z]+\b', l)
             print("re = {}".format(len(words)))
 
