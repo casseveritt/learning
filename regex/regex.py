df1a8ef40da12a73572dfa4c5441998e3cc279ce
Cass Everitt <cass@xyzw.us>
2020-12-22 11:07:47 -0600

    read whole file

--- regex/regex.py
+++ regex/regex.py
@@ -3,15 +3,10 @@ import re
 
 
 def main():
-    with open("gettysburg.txt", "r") as f:
-        i = 0
-        for l in f:
-            i += 1
-            if i >= 20:
-                break
-            words = re.findall(r'\b[A-Za-z]+\b', l)
-            print("re = {}".format(len(words)))
-
+    f = open("gettysburg.txt", "r")
+    s = f.read()
+    f.close()
+    print("file size = {}".format(len(s)))
 
 if __name__ == "__main__":
     main()
