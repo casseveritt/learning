8c322aeb628457762269729c63ab6a685526231a
Cass Everitt <cass@xyzw.us>
2020-12-22 17:26:30 -0600

    code clean-up

--- regex/regex.py
+++ regex/regex.py
@@ -1,43 +1,35 @@
 #!/usr/bin/python3
 import re
+from operator import itemgetter
 from hashlib import sha1
 
 def make_sha1(s, encoding='utf-8'):
     return sha1(s.encode(encoding)).hexdigest()
 
-
+def hist_dict_incr(d, k):
+    if k not in d:
+        d[k] = 0
+    d[k] += 1
 
 def main():
-    f = open("gettysburg.txt", "r")
-    s = f.read()
-    f.close()
-    print("file size = {}".format(len(s)))
+    with open("gettysburg.txt", "r") as f:
+        s = f.read()
 
-    p = re.compile(r'\b(a|an|the)\s([A-Za-z]+)\b', re.MULTILINE|re.IGNORECASE)
+    p = re.compile(r'\b(a|an|the)\s([A-Za-z]+)\b', re.M|re.I)
 
     count = 0
     d = {}
     for m in p.finditer(s):
         count += 1
-        k = m.group(2)
-        if k not in d:
-            d[k] = 0
-        d[k] += 1
+        hist_dict_incr(d, m.group(2))
     
-    top = sorted(d.items(), key = lambda i: -i[1])
-    print("found {} matches\n".format(count))
+    top = sorted(d.items(), reverse=True, key=itemgetter(1))[0:5]
+    print("found {c} matches and {k} unique words\n".format(c=count, k=len(d)))
     print("top five matches:")
-    for v in top[0:5]:
-        print("{}: {}".format(v[0], v[1]))
+    for k, v in top:
+        print("{}: {}".format(k, v))
     
-    words = [t[0] for t in top[0:5]]
-
-    pat = "".join([
-        r'\b(a|an|the)\s+',
-        "({})".format("|".join(words)),
-        r'\b'
-    ])
-    print("pat = {}".format(pat))
+    pat = r'\b(a|an|the)\s+({})\b'.format('|'.join([t[0] for t in top]))
 
     rs = re.sub(pat, lambda m: "{} {}".format(m.group(1), m.group(2).upper()), s)
 
