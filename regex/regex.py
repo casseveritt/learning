a00b0fe9dee4f2081b227503c8d906cb934d2e07
Cass Everitt <cass@xyzw.us>
2020-12-22 12:15:54 -0600

    dict of words following articles

--- regex/regex.py
+++ regex/regex.py
@@ -8,12 +8,20 @@ def main():
     f.close()
     print("file size = {}".format(len(s)))
 
-    p = re.compile(r'\b[A-Za-z]+\b', re.MULTILINE)
+    p = re.compile(r'\b(a|an|the)\s([A-Za-z]+)\b', re.MULTILINE|re.IGNORECASE)
+
     count = 0
+    d = {}
     for m in p.finditer(s):
         count += 1
+        k = m.group(2)
+        if k not in d:
+            d[k] = 0
+        d[k] += 1
     
     print("found {} matches".format(count))
+    for k,v in d.items():
+        print("{}: {}".format(k,v))
 
 if __name__ == "__main__":
     main()
