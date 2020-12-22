085b1165e4e207e460f5570370ef7a4f65eb69be
Cass Everitt <cass@xyzw.us>
2020-12-22 11:28:10 -0600

    compile regex, use finditer

--- regex/regex.py
+++ regex/regex.py
@@ -8,5 +8,12 @@ def main():
     f.close()
     print("file size = {}".format(len(s)))
 
+    p = re.compile(r'\b[A-Za-z]+\b', re.MULTILINE)
+    count = 0
+    for m in p.finditer(s):
+        count += 1
+    
+    print("found {} matches".format(count))
+
 if __name__ == "__main__":
     main()
