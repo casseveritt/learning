875d6c869b1270ee9e0d73e0bc72e13b29fb7b73
Cami Everitt <cami.everitt@gmail.com>
2020-12-23 15:53:52 -0600

    downgrade GLES version to 3.1

--- mineweeper/learning.h
+++ mineweeper/learning.h
@@ -6,7 +6,7 @@
 #else
 #include <GLES3/gl32.h>
 #define GLFW_INCLUDE_NONE
-#define GLSL_VERSION "#version 320 es\n"
+#define GLSL_VERSION "#version 310 es\n"
 #endif
 
 #include <GLFW/glfw3.h>
@@ -19,7 +19,7 @@ inline void make_hints() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #else
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
   glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
 #endif
 }
