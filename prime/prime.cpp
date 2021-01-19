1d58efdf828ac0d8cb12bb7ec5f4b36183a21898
Lucas Everitt <lucas.everitt@gmail.com>
2021-01-19 16:40:56 -0600

    start

--- prime/prime.cpp
+++ prime/prime.cpp
@@ -12,19 +12,23 @@
 int loop = 0;
 
 int main(void) {
-
   int nPrimes = 10;
   std::vector<int> primes;
-  int num = 3; // Start checking with 3
-  while(nPrimes > 0) {
-    
+  int num = 3;  // Start checking with 3
+  while (nPrimes > 0) {
+    bool isPrime = true;
+    for (int i = 2; i < num / 2; i++) {
+    }
+    if (isPrime) {
+      primes.push_back(num);
+      nPrimes--;
+    }
+    num += 2;  // Why bother checking even numbers?
+    loop++;
   }
 
-  for() {
-
+  for () {
   }
-
-  printf("Hello World!\n");
 
   return 0;
 }
