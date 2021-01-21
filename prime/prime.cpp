3e21e12285b95c95ef93d7adef8f39c76c94b2bb
Lucas Everitt <lucas.everitt@gmail.com>
2021-01-21 13:33:56 -0600

    atomic and lock_guard

--- prime/prime.cpp
+++ prime/prime.cpp
@@ -5,6 +5,8 @@
 #include <mutex>
 #include <thread>
 #include <vector>
+#include <atomic>
+#include <bits/stdc++.h> 
 
 #include <cstring>
 
@@ -13,10 +15,10 @@
   sudo apt install libgles2-mesa-dev libglfw3-dev
 */
 
-int nPrimes = 10;
+int nPrimes = 1000;
 std::vector<int> primes;
-int numThreads = 4;
-int num = 3;
+int numThreads = 8;
+std::atomic<int> num (2);
 std::mutex m;
 
 static bool checkPrime(int checkNum) {
@@ -30,70 +32,62 @@ static bool checkPrime(int checkNum) {
   return true;
 }
 
+static int getNextNum() { // returns next number to check
+  if(num.load() == 2) return num.fetch_add(1);
+  return num.fetch_add(2);
+}
+
+static void pushPrime(int primeNum) { // pushes prime number onto list
+  const std::lock_guard<std::mutex> lock(m);
+  primes.push_back(primeNum);
+  nPrimes--;
+}
+
 static void findPrimes() {
-  int checkNum;
-  bool locked = false;
   while (nPrimes > 0) {
-    while (!locked) {
-      locked = m.try_lock();
-    }
-    checkNum = num;
-    num += 2;
-    m.unlock();
-    locked = false;
-
-    bool isPrime = checkPrime(checkNum);
-
-    if (isPrime && nPrimes > 0) {
-      while (!locked) {
-        locked = m.try_lock();  // Tries to lock other threads from altering primes list anc count
-      }
-      primes.push_back(checkNum);
-      nPrimes--;
-      m.unlock();  // Unlocks
-      locked = false;
+    int checkNum = getNextNum();
+    if (checkPrime(checkNum)) {
+      pushPrime(checkNum);
     }
   }
 }
 
-static double GetTimeInSeconds() {
+static double getTimeInSeconds() {
   timespec ts;
   clock_gettime(CLOCK_MONOTONIC, &ts);
   return double(int64_t(ts.tv_sec) * int64_t(1e9) + int64_t(ts.tv_nsec)) * 1e-9;
 }
 
 int main(int argc, char** argv) {
-  double t0 = GetTimeInSeconds();
-
   if (argc >= 2) {
     nPrimes = atoi(argv[1]);
   }
   if (argc == 3) {
-    numThreads = atoi(argv[2]);
+    numThreads = atoi(argv[2]); // Set number of threads to make
   }
-
-  std::vector<std::thread> threads;
   int numPrimes = nPrimes;
 
+  double t0 = getTimeInSeconds(); // Start time
+  std::vector<std::thread> threads;
   threads.resize(numThreads);
-  for (int i = 0; i < numThreads; i++) {
+  for (int i = 0; i < numThreads; i++) { // Initalize threads
     threads[i] = std::thread(findPrimes);
   }
-  for (auto& thread : threads) {
+  for (auto& thread : threads) { // Join threads
     if (thread.joinable()) {
       thread.join();
     }
   }
+  double t1 = getTimeInSeconds(); // End time
 
-  double t1 = GetTimeInSeconds();
-
-  for (int i = 0; i < numPrimes; i++) {
+  sort(primes.begin(), primes.end());
+  for (int i = 0; i < numPrimes; i++) { // Print primes
     printf("%i", primes[i]);
     if (i != numPrimes - 1) {
       printf(", ");
     }
   }
-  printf("\nTime in msec: %lf\n", (t1 - t0) * 1000);
+  printf("\nThreads: %i\nTime in msec: %lf\n", numThreads, (t1 - t0) * 1000);
 
   return 0;
 }
