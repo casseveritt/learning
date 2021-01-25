b58d776aaae90b54d2aaa9f6b0bfedaacc0abf2e
Lucas Everitt <lucas.everitt@gmail.com>
2021-01-25 14:40:33 -0600

    sqrt

--- root/root.cpp
+++ root/root.cpp
@@ -28,7 +28,7 @@ static double getTimeInSeconds() {
   return double(int64_t(ts.tv_sec) * int64_t(1e9) + int64_t(ts.tv_nsec)) * 1e-9;
 }
 
-static double powTen(int n) {
+static double tenPow(int n) {  // 10^n
   double rst = 1.0;
   if (n >= 0) {
     for (int i = 0; i < n; i++) {
@@ -46,24 +46,24 @@ static double sqrRoot(double a) {
   double z = a, rst = 0.0, j = 1.0;
   int max = 24;                    // to define maximum digit
   for (int i = max; i > 0; i--) {  // value must be bigger then 0
-    if (z - ((2 * rst) + (j * powTen(i))) * (j * powTen(i)) >= 0) {
-      while (z - ((2 * rst) + (j * powTen(i))) * (j * powTen(i)) >= 0) {
+    if (z - ((2 * rst) + (j * tenPow(i))) * (j * tenPow(i)) >= 0) {
+      while (z - ((2 * rst) + (j * tenPow(i))) * (j * tenPow(i)) >= 0) {
         j++;
         if (j >= 10) break;
       }
       j--;                                                   // correct the extra value by minus one to j
-      z -= ((2 * rst) + (j * powTen(i))) * (j * powTen(i));  // find value of z
-      rst += j * powTen(i);                                  // find sum of a
+      z -= ((2 * rst) + (j * tenPow(i))) * (j * tenPow(i));  // find value of z
+      rst += j * tenPow(i);                                  // find sum of a
       j = 1.0;
     }
   }
 
   for (int i = 0; i >= 0 - max; i--) {
-    if (z - ((2 * rst) + (j * powTen(i))) * (j * powTen(i)) >= 0) {
-      while (z - ((2 * rst) + (j * powTen(i))) * (j * powTen(i)) >= 0) j++;
+    if (z - ((2 * rst) + (j * tenPow(i))) * (j * tenPow(i)) >= 0) {
+      while (z - ((2 * rst) + (j * tenPow(i))) * (j * tenPow(i)) >= 0) j++;
       j--;
-      z -= ((2 * rst) + (j * powTen(i))) * (j * powTen(i));  // find value of z
-      rst += j * powTen(i);                                  // find sum of a
+      z -= ((2 * rst) + (j * tenPow(i))) * (j * tenPow(i));  // find value of z
+      rst += j * tenPow(i);                                  // find sum of a
       j = 1.0;
     }
   }
@@ -84,17 +84,17 @@ int main(int argc, char** argv) {
     }
   }
 
+  double executeTime0, executeTime1;
+
   t0 = getTimeInSeconds();  // Start time
   double rootOut = sqrRoot(num);
-  /*for (int i = 0; i < numThreads; i++) {  // Initalize threads
-    threads[i] = std::thread(findPrimes);
-  }
-  for (auto& thread : threads) {  // Join threads
-    if (thread.joinable()) {
-      thread.join();
-    }
-  }*/
-  t2 = getTimeInSeconds();  // End time
+  t1 = getTimeInSeconds();  // End time
+  executeTime0 = (t1 - t0) * 1000;
+
+  t0 = getTimeInSeconds();  // Start time
+  double mathRoot = sqrt(num);
+  t1 = getTimeInSeconds();  // End time
+  executeTime1 = (t1 - t0) * 1000;
 
   /*for (int i = 0; i < numPrimes; i++) {  // Print primes
     printf("%i", primes[i]);
@@ -103,7 +103,8 @@ int main(int argc, char** argv) {
     }
   }*/
   // if ((int)primes.size() < numPrimes) printf("\nTimed out\nTotal primes found: %i", (int)primes.size());
-  printf("Number: %lf\nRoot:\t\t%.24lf\nMath.h Root:\t%.24lf\nTime in msec: %lf\n", num, rootOut, sqrt(num), (t2 - t0) * 1000);
+  printf("Number:\t%lf\nRoot:\t%lf\nMath.h:\t%lf\n\n", num, rootOut, mathRoot);
+  printf("My time in msec:   %lf\nMath time in msec: %lf\n", executeTime0, executeTime1);
 
   return 0;
 }
