79de7a45440988a7787129e3128a1ed8b8f2477b
Cami Everitt <cami.everitt@gmail.com>
2021-01-26 19:11:02 -0600

    prime WORKING

--- prime/prime.cpp
+++ prime/prime.cpp
@@ -33,7 +33,10 @@ static bool isPrime(int number) {
     if (number == 2) {
       return true;
     }
-    for (int i = 3; i < sqrt(number); i = i + 2) {
+    if ((number&1) == 0) {
+      return false;
+    }
+    for (int i = 2; i <= sqrt(number); i++) {
       if (number % i == 0) {
         return false;
       }
