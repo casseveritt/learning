46236f9dce41f3573ae9ef4a91e0a72716605996
Lucas Everitt <lucas.everitt@gmail.com>
2021-01-27 15:09:56 -0600

    cos and tan

--- trig/trig.cpp
+++ trig/trig.cpp
@@ -17,6 +17,7 @@
   sudo apt install libgles2-mesa-dev libglfw3-dev
 */
 
+float num = 90;
 double t0, t1;
 
 static double getTimeInSeconds() {
@@ -41,17 +42,41 @@ static int factorial(int n) {
   return out;
 }
 
-static float tsin(float theta) {
-  float sineTheta = theta;
+static float tsin(float t) {
+  float theta = t;
+  float sinTheta = theta;
   bool sign = false;
-  for (int i = 3; i < 20; i += 2) {
+  for (int i = 3; i < 14; i += 2) {
+    double numPow = pow(theta, i);
+    double numFac = factorial(i);
+    // printf("Pow: %f\nFac: %f\n",numPow, numFac);
     if (sign)
-      sineTheta += (pow(theta, i) / factorial(i));
+      sinTheta += (numPow / numFac);
     else
-      sineTheta -= (pow(theta, i) / factorial(i));
+      sinTheta -= (numPow / numFac);
     sign = !sign;
   }
-  return sineTheta;
+  return sinTheta;
+}
+
+static float tcos(float t) {
+  float theta = t;
+  double cosTheta = 1;
+  bool sign = false;
+  for (int i = 2; i < 13; i += 2) {
+    double numPow = pow(theta, i);
+    double numFac = factorial(i);
+    if (sign)
+      cosTheta += (numPow / numFac);
+    else
+      cosTheta -= (numPow / numFac);
+    sign = !sign;
+  }
+  return cosTheta;
+}
+
+static float ttan(float theta) {
+  return tsin(theta) / tcos(theta);
 }
 
 int main(int argc, char** argv) {
@@ -59,7 +84,7 @@ int main(int argc, char** argv) {
   while ((opt = getopt(argc, argv, "n:")) != -1) {
     switch (opt) {
       case 'n':
-        // if (optarg) num = atof(optarg);
+        if (optarg) num = atof(optarg);
         break;
       default: /* '?' */
         fprintf(stderr, "Usage: %s [-a algorithm (l|c|b)] [-n numPrimes] [-t numThreads]\n", argv[0]);
@@ -67,9 +92,12 @@ int main(int argc, char** argv) {
     }
   }
 
-  float theta = 40;
+  float radNum = toRadians(num);
 
-  printf("Theta: %f\n\nMy Sin:  %.10f\nMath Sin:%.10f\n", theta, tsin(toRadians(theta)), sin(toRadians(theta)));
+  printf("Theta: %f\n\n", num);
+  printf("My Sin:  %f\nMath Sin:%f\n\n", tsin(radNum), sin(radNum));
+  printf("My Cos:  %f\nMath Cos:%f\n\n", tcos(radNum), cos(radNum));
+  printf("My Tan:  %f\nMath Tan:%f\n", ttan(radNum), tan(radNum));
 
   return 0;
 }
