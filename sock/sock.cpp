b326e4e7661fb72b8a5aeefcc9f2a4b99f40c5ab
Cass Everitt <cass@xyzw.us>
2021-02-07 16:38:55 -0600

    fix some pointer business

--- sock/sock.cpp
+++ sock/sock.cpp
@@ -31,7 +31,16 @@ static double getTimeInSeconds() {
 }
 
 int main(int argc, char** argv) {
-  if ((string)argv[0] == "./server") isServer = true;
+  isServer = strcmp(argv[0], "./server") == 0;
+
+  const char* hostname = "localhost";
+  if (argc == 2) {
+    hostname = argv[1];
+  }
+  const char* message = "echo";
+  if (argc == 3) {
+    message = argv[2];
+  }
 
   if (isServer) {
     printf("Server created!\n");
@@ -41,18 +50,19 @@ int main(int argc, char** argv) {
     printf("Waiting...\n");
     s = server.Accept();
     printf("Connected!\n");
-    char* input;
-    s.Read(*input);
+    char input[100];
+    int bytes = s.ReadPartial(input, 100);
     printf("Recieved '%s'\nSent back.\n", input);
-    s.Write(input);
+    s.Write(input, bytes);
   } else {
     printf("Client created!\n");
     printf("Attempting connection...\n");
-    s.Connect(GetIpAddress("luc"), 2113);
-    printf("Connected!\nSending 'Echo'\n");
-    s.Write("Echo");
-    char* input;
-    s.Read(*input);
+    s.Connect(GetIpAddress(hostname), 2113);
+    printf("Connected!\nSending '%s'\n", message);
+    bool success = s.Write(message, strlen(message) + 1);
+    printf("Write to socket %s.\n", success ? "succeeded" : "failed");
+    char input[100];
+    int bytes = s.Read(input, 100);
     printf("Receiving '%s'\n", input);
   }
 
