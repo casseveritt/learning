7960268a9f448dd3f8e47d65f786690cc107b9e1
Lucas Everitt <lucas.everitt@gmail.com>
2021-02-06 21:32:10 -0600

    Port id trouble

--- sock/sock.cpp
+++ sock/sock.cpp
@@ -21,14 +21,29 @@ using namespace std;
 */
 
 bool isServer = false;
+double t0, t1;
 Socket s;
+
+static double getTimeInSeconds() {
+  timespec ts;
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return double(int64_t(ts.tv_sec) * int64_t(1e9) + int64_t(ts.tv_nsec)) * 1e-9;
+}
 
 int main(int argc, char** argv) {
   if ((string)argv[0] == "./server") isServer = true;
 
   if (isServer) {
     printf("Server created!\n");
-    // while ()
+    Listener server;
+    t0 = getTimeInSeconds();
+    while (server.Listen(127.0.0.1) == false) {
+      if (getTimeInSeconds() - t0 >= 10) {
+        printf("Waiting...\n");
+        t0 = getTimeInSeconds();
+      }
+    }
+    s = server.Accept(127.0.0.1);
   } else {
     printf("Client created!\n");
   }
