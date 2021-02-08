a78bd7786f895a044b5d4c813da9186d31d3a8a5
Lucas Everitt <lucas.everitt@gmail.com>
2021-02-08 17:15:01 -0600

    Sock additions

--- sock/sock.cpp
+++ sock/sock.cpp
@@ -20,10 +20,15 @@ using namespace std;
   sudo apt install libgles2-mesa-dev libglfw3-dev
 */
 
-bool isServer = false;
+const char* hostname = "localhost";
+bool shouldClose = false, isServer = false;
+int port = 2113;
 double t0, t1;
 Socket s;
-int port = 2113;
+
+Listener server;
+vector<Socket> connections;
+int connectionLimit = 4;
 
 static double getTimeInSeconds() {
   timespec ts;
@@ -31,48 +36,87 @@ static double getTimeInSeconds() {
   return double(int64_t(ts.tv_sec) * int64_t(1e9) + int64_t(ts.tv_nsec)) * 1e-9;
 }
 
+// Server
+
+static void serverCommand(char* command, Socket& sock) {
+  if (strcmp(command, "quit") == 0) {
+    sock.Close();
+    for (const int i = 0; i < connections.size(); i++) {
+      if (connections[i] == sock) {
+        sock.Close();
+        connections.erase(i);
+      }
+    }
+    if (connections.size() == 0) shouldClose = true;
+  }
+}
+
+static void makeConnections() {
+  while (!shouldClose) {
+    server.Listen(port);
+    if (int(connections.size()) < connectionLimit) {
+      connections.push_back(server.Accept());
+      connections.back().SetNonblocking();
+      printf("Connection made!\n");
+    } else
+      printf("Connection attempted! (Full)\n");
+  }
+}
+
+static void serverMain() {
+  server.SetNonblocking();
+
+  printf("Server created!\n");
+  t0 = getTimeInSeconds();
+  thread connect = thread(makeConnections);
+  int bytes = 100;
+  while (!shouldClose) {
+    char input[bytes];
+    for (Socket conn : connections) {
+      if (conn.CanRead()) {
+        conn.ReadPartial(input, bytes);
+        printf("Input: '%s'\n", input);
+        serverCommand(input, conn);
+      }
+    }
+  }
+  for (Socket conn : connections) conn.Close();
+}
+
+// Client
+
+static void clientCommand(string command) {
+  const char* comm;
+  if (command == "quit") {
+    comm = "quit";
+    s.Write(comm, strlen(comm) + 1);
+    shouldClose = true;
+  }
+}
+
+static void clientMain() {
+  printf("Client created!\n");
+  printf("Attempting connection...\n");
+  s.Connect(GetIpAddress(hostname), port);
+  printf("Connected!\n");
+  string command;
+  while (!shouldClose) {
+    cin >> command;
+    clientCommand(command);
+  }
+}
+
+// Main
+
 int main(int argc, char** argv) {
   isServer = strcmp(argv[0], "./server") == 0;
 
-  const char* hostname = "localhost";
-  if (argc >= 2) {
-    hostname = argv[1];
-  }
-  const char* message = "echo";
-  if (argc == 3) {
-    message = argv[2];
-  }
+  if (argc >= 2) hostname = argv[1];
 
-  if (isServer) {
-    printf("Server created!\n");
-    Listener server;
-    t0 = getTimeInSeconds();
-    server.Listen(port);
-    printf("Waiting...\n");
-    s = server.Accept();
-    printf("Connected!\n");
-    char input[100];
-    int bytes = s.ReadPartial(input, 100);
-    printf("Received '%s'\n", input);
-    if ((string)input == (string)"quit") {
-      message = "Quitting";
-      s.Write(message, strlen(message) + 1);
-      s.Close();
-    } else {
-      message = "Invalid command";
-      s.Write(message, strlen(message) + 1);
-    }
-  } else {
-    printf("Client created!\n");
-    printf("Attempting connection...\n");
-    s.Connect(GetIpAddress(hostname), port);
-    printf("Connected!\nSending '%s'\n", message);
-    bool success = s.Write(message, strlen(message) + 1);
-    printf("Write to socket %s.\n", success ? "succeeded" : "failed");
-    char input[100];
-    int bytes = s.Read(input, 100);
-    printf("Receiving '%s'\n", input);
-  }
+  if (isServer)
+    serverMain();
+  else
+    clientMain();
 
   return 0;
 }
