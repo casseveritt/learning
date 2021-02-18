9f2ead339b5e151935c3b0410a58de0a118da153
Lucas Everitt <lucas.everitt@gmail.com>
2021-02-18 16:29:00 -0600

    Image blocks

--- dct/dct.cpp
+++ dct/dct.cpp
@@ -36,10 +36,26 @@ int main(int argc, char** argv) {
       uint8_t a;
     };
   };
-  int size = w * h;
+  //int size = w * h;
   pixel* pix = reinterpret_cast<pixel*>(img);
-  for (int i = 0; i < size; i++) {
-    pix[i].r = 0;
+  for (int i = 0; i < h; i+=8) {
+ 	for (int j = 0; j < w; j+=8) {
+ 		pixel avgCol = pix[(i*w)+j];
+		for(int l=1;l<64;l++) {
+			int x = l%8, y = l/8;
+    		avgCol.r = (avgCol.r + pix[((i+y)*w)+(j+x)].r)/2;
+    		avgCol.g = (avgCol.g + pix[((i+y)*w)+(j+x)].g)/2;
+    		avgCol.b = (avgCol.b + pix[((i+y)*w)+(j+x)].b)/2;
+    		avgCol.a = (avgCol.a + pix[((i+y)*w)+(j+x)].a)/2;
+  		}
+  		for(int l=0;l<64;l++) {
+			int x = l%8, y = l/8;
+    		pix[((i+y)*w)+(j+x)].r = avgCol.r;
+    		pix[((i+y)*w)+(j+x)].g = avgCol.g;
+    		pix[((i+y)*w)+(j+x)].b = avgCol.b;
+    		pix[((i+y)*w)+(j+x)].a = avgCol.a;
+  		}
+  	}
   }
   image_store_png("out.png", w, h, 4, img);
 }
