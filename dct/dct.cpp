fed459f630b5439bffc1a6bbb9bfa2d613acb6f9
Lucas Everitt <lucas.everitt@gmail.com>
2021-02-18 19:40:37 -0600

    dct progress

--- dct/dct.cpp
+++ dct/dct.cpp
@@ -15,6 +15,9 @@ int main(int argc, char** argv) {
   string imgName = "Lenna.png";
   if (argc >= 2) imgName = argv[1];
 
+  int mbd = 8;          // Macroblock dimensions
+  int mbv = mbd * mbd;  // Macroblock volume
+
   int w, h, n;
   const char* im = "images/";
   char* imgLocation = new char[strlen(im) + imgName.size() + 1];
@@ -25,7 +28,7 @@ int main(int argc, char** argv) {
 
   img = image_load(imgLocation, &w, &h, &n);
   if (n != 4) {
-    printf("warning: need 4-component pixels. n=%d\n", n);
+    // printf("warning: need 4-component pixels. n=%d\n", n);
   }
   union pixel {
     uint32_t i;
@@ -36,26 +39,33 @@ int main(int argc, char** argv) {
       uint8_t a;
     };
   };
-  //int size = w * h;
   pixel* pix = reinterpret_cast<pixel*>(img);
-  for (int i = 0; i < h; i+=8) {
- 	for (int j = 0; j < w; j+=8) {
- 		pixel avgCol = pix[(i*w)+j];
-		for(int l=1;l<64;l++) {
-			int x = l%8, y = l/8;
-    		avgCol.r = (avgCol.r + pix[((i+y)*w)+(j+x)].r)/2;
-    		avgCol.g = (avgCol.g + pix[((i+y)*w)+(j+x)].g)/2;
-    		avgCol.b = (avgCol.b + pix[((i+y)*w)+(j+x)].b)/2;
-    		avgCol.a = (avgCol.a + pix[((i+y)*w)+(j+x)].a)/2;
-  		}
-  		for(int l=0;l<64;l++) {
-			int x = l%8, y = l/8;
-    		pix[((i+y)*w)+(j+x)].r = avgCol.r;
-    		pix[((i+y)*w)+(j+x)].g = avgCol.g;
-    		pix[((i+y)*w)+(j+x)].b = avgCol.b;
-    		pix[((i+y)*w)+(j+x)].a = avgCol.a;
-  		}
-  	}
+  for (int i = 0; i < h; i += mbd) {
+    for (int j = 0; j < w; j += mbd) {
+      pixel avgCol = pix[(i * w) + j];
+      for (int l = 1; l < mbv; l++) {
+        int x = l % mbd, y = l / mbd;
+        int coords = ((i + y) * w) + (j + x);
+        avgCol.r = (avgCol.r + pix[coords].r) / 2;
+        avgCol.g = (avgCol.g + pix[coords].g) / 2;
+        avgCol.b = (avgCol.b + pix[coords].b) / 2;
+        avgCol.a = (avgCol.a + pix[coords].a) / 2;
+        float cx, cy;
+        if (x == 0)
+          cx = 1 / sqrt(mbd);
+        else
+          cx = sqrt(2) / sqrt(mbd);
+        if (y == 0)
+          cy = 1 / sqrt(mbd);
+        else
+          cy = sqrt(2) / sqrt(mbd);
+        // dct[x][y] = cx * cy (sum(k=0 to m-1) sum(l=0 to n-1) pix[coords] * cos((2*k+1) *i*pi/2*m) * cos((2*l+1) *j*pi/2*n)
+      }
+      for (int l = 0; l < mbv; l++) {
+        int x = l % mbd, y = l / mbd;
+        pix[((i + y) * w) + (j + x)] = avgCol;
+      }
+    }
   }
   image_store_png("out.png", w, h, 4, img);
 }
