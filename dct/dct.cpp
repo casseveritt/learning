8b0d321c2ba8993f4ec9452f8816750eb0801bcb
Cass Everitt <casseveritt@fb.com>
2021-02-27 18:29:12 -0600

    rewrote transforms

--- dct/dct.cpp
+++ dct/dct.cpp
@@ -28,79 +28,79 @@ struct RGBA32F {
 
 template <typename T> struct Block8x8 {
    T element[8][8];
+   T& el(int i, int j) { return element[j][i]; }
+   T el(int i, int j) const { return element[j][i]; }
 };
 
-static Block8x8<RGBA32F> DCTransform(Block8x8<RGBA8> inMat) {
-	Block8x8<RGBA32F> dctMat;
+static float dct2(int n, int k) {
+	return (n == 0 ? 1.0f / sqrt(2.0f) : 1.0f ) * 0.5f * cos( (M_PI / 8.0) * (n + 0.5) * k);
+}
+
+static float dct3(int n, int k) {
+	if (n == 0) {
+		return 0.5 / sqrt(2.0f);
+	}
+	return 0.5f * cos( (M_PI / 8.0) * n * (k + 0.5));
+}
+
+
+static Block8x8<RGBA32F> DCTransform(Block8x8<RGBA8> spatialdom) {
+	Block8x8<RGBA32F> freqdom;
 
 	for (int u=0;u<8;++u) {
 		for (int v=0;v<8;++v) {
-			double Cu, Cv;
-			double sumr, sumg, sumb, suma;
-			sumr = sumg = sumb = suma = 0.0;
-
-			if (u == 0) Cu = 1.0 / sqrt(2.0);
-			else Cu = 1.0;
-			if (v == 0) Cv = 1.0 / sqrt(2.0);
-			else Cv = (1.0);  
+			RGBA32F s = {};
 
 			for (int i=0;i<8;i++) {
+				float ci = dct2(i, u);
 				for (int j=0;j<8;j++) {
-					double cosineWave = cos((2 * i + 1) * u * PI / 16.0) * cos((2 * j + 1) * v * PI / 16.0);
-                    // Level around 0
-					RGBA8 pixel = inMat.element[i][j];
-					sumr += pixel.r*cosineWave;
-					sumg += pixel.g*cosineWave;
-					sumb += pixel.b*cosineWave;
-					suma += pixel.a*cosineWave;
+					auto c = ci * dct2(j, v);
+					auto& pix = spatialdom.el(i,j);
+					s.r += pix.r * c;
+					s.g += pix.g * c;
+					s.b += pix.b * c;
+					s.a += pix.a * c;
 				}
 			}
-			dctMat.element[u][v].r = (0.25 * Cu * Cv * sumr);
-			dctMat.element[u][v].g = (0.25 * Cu * Cv * sumg);
-			dctMat.element[u][v].b = (0.25 * Cu * Cv * sumb);
-			dctMat.element[u][v].a = (0.25 * Cu * Cv * suma);
+			freqdom.el(u,v) = s;
 		}
 	}
-	return dctMat;
+	return freqdom;
 };
 
-static Block8x8<RGBA8> IDCTransform(Block8x8<RGBA32F> dctMat) {
-	Block8x8<RGBA8> outMat;
+static Block8x8<RGBA8> IDCTransform(Block8x8<RGBA32F> freqdom) {
+	Block8x8<RGBA8> spatialdom;
 
 	for (int i=0;i<8;++i) {
-		for (int j=0;j<8;++j) { 
-			double Cu, Cv;
-			double sumr, sumg, sumb, suma;
-			sumr = sumg = sumb = suma = 0.0;
+		for (int j=0;j<8;++j) {
+			RGBA32F s = {};
 
 			for (int u=0;u<8;u++) {
+				float cu = dct3(u, i);
 				for (int v=0;v<8;v++) {
-					double cosineWave = cos((2 * i + 1) * u * PI / 16.0) * cos((2 * j + 1) * v * PI / 16.0);
+					float c =  cu * dct3(v, j);
 
-					if (u == 0) Cu = 1.0;
-					else Cu = 1.0 / sqrt(2.0);
-					if (v == 0) Cv = 1.0;
-					else Cv = (1.0 / sqrt(2.0));
-
-					// Level around 0
-
-					RGBA32F dctElement = dctMat.element[u][v];
-					sumr += dctElement.r * cosineWave;
-					sumg += dctElement.g * cosineWave;
-					sumb += dctElement.b * cosineWave;
-					suma += dctElement.a * cosineWave;
+					const auto& pix = freqdom.el(u, v);
+					s.r += pix.r * c;
+					s.g += pix.g * c;
+					s.b += pix.b * c;
+					s.a += pix.a * c;
 				}
 			}
-			outMat.element[i][j].r = (0.25 * Cu * Cv * sumr);
-			outMat.element[i][j].g = (0.25 * Cu * Cv * sumg);
-			outMat.element[i][j].b = (0.25 * Cu * Cv * sumb);
-			outMat.element[i][j].a = (0.25 * Cu * Cv * suma);
+			RGBA8 si;
+			si.r = std::max(0.0f, std::min(255.0f, s.r));
+			si.g = std::max(0.0f, std::min(255.0f, s.g));
+			si.b = std::max(0.0f, std::min(255.0f, s.b));
+			si.a = std::max(0.0f, std::min(255.0f, s.a));
+
+			spatialdom.el(i,j) = si;
 		}
 	}
-	return outMat;
+	return spatialdom;
 };
 
 int main(int argc, char** argv) {
+
   string imgName = "Lenna.png";
   if (argc >= 2) imgName = argv[1];
 
@@ -119,16 +119,16 @@ int main(int argc, char** argv) {
   RGBA8* pix = reinterpret_cast<RGBA8*>(img);
   for (int i = 0; i < h; i += 8) {
     for (int j = 0; j < w; j += 8) {
-      Block8x8<RGBA8> macroblock;
+      Block8x8<RGBA8> b;
       for (int ii = 0; ii < 8; ii++) {
       	for(int jj = 0; jj < 8; jj++) {
-	        macroblock.element[jj][ii] = pix[((i + ii) * w) + (j + jj)];
+	        b.el(ii,jj) = pix[((i + ii) * w) + (j + jj)];
       	}
       }
-      macroblock = IDCTransform(DCTransform(macroblock));
+      b = IDCTransform(DCTransform(b));
       for (int ii = 0; ii < 8; ii++) {
       	for(int jj = 0; jj < 8; jj++) {
-	        pix[((i + ii) * w) + (j + jj)] = macroblock.element[jj][ii];
+	        pix[((i + ii) * w) + (j + jj)] = b.el(ii,jj);
 	    }
       }
     }
