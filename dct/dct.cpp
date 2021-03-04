f49bbb2b0c2f90fee811a5c9d8a299a03eaa97cc
Lucas Everitt <lucas.everitt@gmail.com>
2021-03-04 15:28:53 -0600

    dct: General cleanup

--- dct/dct.cpp
+++ dct/dct.cpp
@@ -38,14 +38,40 @@ struct RGBA8 {
       uint8_t r, g, b, a;
     };
   };
+  RGBA8() {
+    i = 0;
+  }
+  RGBA8(uint32_t iin) {
+    i = iin;
+  }
+  RGBA8(uint8_t rin, uint8_t gin, uint8_t bin) {
+    r = rin, g = gin, b = bin, a = 255;
+  }
+  RGBA8(uint8_t rin, uint8_t gin, uint8_t bin, uint8_t ain) {
+    r = rin, g = gin, b = bin, a = ain;
+  }
 };
 
 struct YCbCr32F {
   float y, cb, cr;
+  YCbCr32F() {
+    y = cb = cr = 0.0f;
+  }
+  YCbCr32F(float yin, float cbin, float crin) {
+    y = yin, cb = cbin, cr = crin;
+  }
 };
+
+enum MFlag { Turn, Up, Down };
 
 struct RLEJam {
   int v, l;
+  RLEJam() {
+    v = l = 0;
+  }
+  RLEJam(int vin, int lin) {
+    v = vin, l = lin;
+  }
 };
 
 template <typename RGBA>
@@ -99,6 +125,10 @@ struct Block8x8 {
   }
 };
 
+static float FToUint8t(float number) {
+  return max(0.0f, min(255.0f, number));
+};
+
 static Block8x8<YCbCr32F> RGBtoYCBCR(Block8x8<RGBA8> in) {
   Block8x8<YCbCr32F> out = {};
   YCbCr32F ycbcr;
@@ -117,16 +147,11 @@ static Block8x8<YCbCr32F> RGBtoYCBCR(Block8x8<RGBA8> in) {
 
 static Block8x8<RGBA8> YCBCRtoRGB(Block8x8<YCbCr32F> in) {
   Block8x8<RGBA8> out = {};
-  RGBA8 rgb;
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
       YCbCr32F ycbcr = in.el(i, j);
       Vec4f rgbVec = RGBfromYCBCR * Vec4f(ycbcr.y, ycbcr.cb, ycbcr.cr, 1.0f);
-      rgb.r = max(0.0f, min(255.0f, rgbVec.x));
-      rgb.g = max(0.0f, min(255.0f, rgbVec.y));
-      rgb.b = max(0.0f, min(255.0f, rgbVec.z));
-      rgb.a = 255;
-      out.el(i, j) = rgb;
+      out.el(i, j) = RGBA8(FToUint8t(rgbVec.x), FToUint8t(rgbVec.y), FToUint8t(rgbVec.z));
     }
   }
   return out;
@@ -145,9 +170,7 @@ static void BlockSplitter(Block8x8<YCbCr32F> ycbcr, Block8x8<float>* yb, Block8x
 static void BlockJoiner(Block8x8<YCbCr32F>* ycbcr, Block8x8<float> yb, Block8x8<float> cbb, Block8x8<float> crb) {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
-      ycbcr->el(i, j).y = yb.el(i, j);
-      ycbcr->el(i, j).cb = cbb.el(i, j);
-      ycbcr->el(i, j).cr = crb.el(i, j);
+      ycbcr->el(i, j) = YCbCr32F(yb.el(i, j), cbb.el(i, j), crb.el(i, j));
     }
   }
 };
@@ -159,21 +182,18 @@ static float dct2(int n, int k) {
 
 static Block8x8<float> DCTransform(Block8x8<float> spatialdom) {
   Block8x8<float> freqdom;
-
   for (int u = 0; u < 8; ++u) {
     float cu = (u == 0) ? 0.5f / sqrt(2.0f) : 0.5f;
     for (int v = 0; v < 8; ++v) {
       float cv = (v == 0) ? 0.5f / sqrt(2.0f) : 0.5f;
       float s = 0.0f;
-
       for (int i = 0; i < 8; i++) {
         float ci = dct2<8>(i, u);
         for (int j = 0; j < 8; j++) {
           s += spatialdom.el(i, j) * (ci * dct2<8>(j, v));
         }
       }
-      s *= cu * cv;
-      freqdom.el(u, v) = s;
+      freqdom.el(u, v) = (s * cu * cv);
     }
   }
   return freqdom;
@@ -181,11 +201,9 @@ static Block8x8<float> DCTransform(Block8x8<float> spatialdom) {
 
 static Block8x8<float> IDCTransform(Block8x8<float> freqdom) {
   Block8x8<float> spatialdom;
-
   for (int i = 0; i < 8; ++i) {
     for (int j = 0; j < 8; ++j) {
       float s = 0.0f;
-
       for (int u = 0; u < 8; u++) {
         float cu = (u == 0) ? 0.5 / sqrt(2.0f) : 0.5;
         for (int v = 0; v < 8; v++) {
@@ -199,91 +217,86 @@ static Block8x8<float> IDCTransform(Block8x8<float> freqdom) {
   return spatialdom;
 };
 
+// Divides the DCT matrix element by the corresponding quant matrix element.
 static void Quantization(Block8x8<float>* dct) {
   for (int i = 0; i < 8; ++i) {
     for (int j = 0; j < 8; ++j) {
-      // Divides the DCT matrix element by the corresponding quant matrix element.
       dct->el(i, j) = round(dct->el(i, j) / quantMat[i][j]);
     }
   }
 };
 
+// Multiplies the quantized matrix element by the corresponding quant matrix element.
 static void Unquantize(Block8x8<float>* quantdct) {
   for (int i = 0; i < 8; ++i) {
     for (int j = 0; j < 8; ++j) {
-      // Multiplies the quantized matrix element by the corresponding quant matrix element.
       quantdct->el(i, j) = round(quantdct->el(i, j) * quantMat[i][j]);
     }
   }
 };
 
 static vector<RLEJam> RLEncoding(Block8x8<float> quantizedMat) {
-  int r = 0, c = 0, f = 0;  // Row, Column, and increment behavior flag
-  int num = quantizedMat.el(r, c), length = 0;
+  int r = 0, c = 0;  // Row, Column
+  MFlag f = Turn;    // Movement behavior
   vector<RLEJam> out;
-  RLEJam jam;
+  RLEJam jam(quantizedMat.el(r, c), 0);
   for (int i = 0; i < 64; i++) {
-    if (num == quantizedMat.el(r, c))
-      length++;
+    if (jam.v == quantizedMat.el(r, c))
+      jam.l++;
     else {
-      jam.v = num;
-      jam.l = length;
       out.push_back(jam);
-      num = quantizedMat.el(r, c);
-      length = 1;
+      jam = RLEJam(quantizedMat.el(r, c), 1);
     }
     switch (f) {
-      case 0:              // Hit wall
+      case Turn:           // Hit wall
         if (r % 7 == 0) {  // Top or bottom wall
-          if (r == 0) c++, f = -1;
-          if (r == 7) c++, f = 1;
+          if (r == 0) c++, f = Down;
+          if (r == 7) c++, f = Up;
         } else {  // Side wall
-          if (c == 0) r++, f = 1;
-          if (c == 7) r++, f = -1;
+          if (c == 0) r++, f = Up;
+          if (c == 7) r++, f = Down;
         }
         break;
-      case 1:  // Up-Right diagonal
+      case Up:  // Up-Right diagonal
         r--, c++;
-        if (r % 7 == 0 || c % 7 == 0) f = 0;
+        if (r % 7 == 0 || c % 7 == 0) f = Turn;
         break;
-      case -1:  // Down-Left diagonal
+      case Down:  // Down-Left diagonal
         r++, c--;
-        if (r % 7 == 0 || c % 7 == 0) f = 0;
+        if (r % 7 == 0 || c % 7 == 0) f = Turn;
         break;
       default:
         break;
     }
   }
-  jam.v = num;
-  jam.l = length;
   out.push_back(jam);
   return out;
 };
 
 static Block8x8<float> RLDecoding(vector<RLEJam> encodedBlock) {
-  int r = 7, c = 7, f = 0;  // Row, Column, and increment behavior flag
+  int r = 0, c = 0, size = encodedBlock.size();  // Row, Column, and vector size
+  MFlag f = Turn;                                // Movement behavior
   Block8x8<float> out;
-  for (int i = encodedBlock.size() - 1; i >= 0; i--) {
-    RLEJam jam = encodedBlock[i];
-    for (int j = 0; j < jam.l; j++) {
-      out.element[c][r] = jam.v;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < encodedBlock[i].l; j++) {
+      out.element[c][r] = encodedBlock[i].v;
       switch (f) {
-        case 0:              // Hit wall
+        case Turn:           // Hit wall
           if (r % 7 == 0) {  // Top or bottom wall
-            if (r == 0) c--, f = -1;
-            if (r == 7) c--, f = 1;
+            if (r == 0) c++, f = Down;
+            if (r == 7) c++, f = Up;
           } else {  // Side wall
-            if (c == 0) r--, f = 1;
-            if (c == 7) r--, f = -1;
+            if (c == 0) r++, f = Up;
+            if (c == 7) r++, f = Down;
           }
           break;
-        case 1:  // Up-Right diagonal
+        case Up:  // Up-Right diagonal
           r--, c++;
-          if (r % 7 == 0 || c % 7 == 0) f = 0;
+          if (r % 7 == 0 || c % 7 == 0) f = Turn;
           break;
-        case -1:  // Down-Left diagonal
+        case Down:  // Down-Left diagonal
           r++, c--;
-          if (r % 7 == 0 || c % 7 == 0) f = 0;
+          if (r % 7 == 0 || c % 7 == 0) f = Turn;
           break;
         default:
           break;
@@ -295,35 +308,31 @@ static Block8x8<float> RLDecoding(vector<RLEJam> encodedBlock) {
 
 int main(int argc, char** argv) {
   // Setting color space transform matricies
-  YCBCRfromRGB.SetRow(0, Vec4f(0.258, 0.508, 0.0937, 16.0));
-  YCBCRfromRGB.SetRow(1, Vec4f(-0.148, -0.289, 0.445, 128.0));
-  YCBCRfromRGB.SetRow(2, Vec4f(0.445, -0.367, -0.071, 128.0));
-  YCBCRfromRGB.SetRow(3, Vec4f(0.0, 0.0, 0.0, 1.0));
+  YCBCRfromRGB =
+      Matrix4f(0.258, 0.508, 0.0937, 16.0, -0.148, -0.289, 0.445, 128.0, 0.445, -0.367, -0.071, 128.0, 0.0, 0.0, 0.0, 1.0);
   RGBfromYCBCR = YCBCRfromRGB.Inverted();
 
-  string imgName = "Lenna.png";
-  if (argc >= 2) imgName = argv[1];
-
+  char* imgName = "images/Lenna.png";
+  if (argc >= 2) {
+    imgName = "images/";
+    strcat(imgName, argv[1]);
+  }
+  char* imgLocation = new char[sizeof(imgName) + 1];
+  strcpy(imgLocation, imgName);
   int w, h, n;
-  const char* im = "images/";
-  char* imgLocation = new char[strlen(im) + imgName.size() + 1];
-  strcpy(imgLocation, im);
-  strcat(imgLocation, imgName.c_str());
 
   unsigned char* img;
-
   img = image_load(imgLocation, &w, &h, &n);
-  if (n != 4) {
-    // printf("warning: need 4-component pixels. n=%d\n", n);
-  }
-  RGBA8* pix = reinterpret_cast<RGBA8*>(img);
+
+  auto pix = reinterpret_cast<RGBA8*>(img);
   auto dif = new RGBA8[w * h];
+
   for (int i = 0; i < h; i += 8) {
     for (int j = 0; j < w; j += 8) {
       Block8x8<RGBA8> b;
       Block8x8<YCbCr32F> ctb;
-      Block8x8<float> yBlock, cbBlock, crBlock;
-      vector<RLEJam> yEBlock, cbEBlock, crEBlock;
+      vector<Block8x8<float>> channels(3);
+      vector<vector<RLEJam>> encodedCh(3);
       for (int ii = 0; ii < 8; ii++) {
         for (int jj = 0; jj < 8; jj++) {
           b.el(ii, jj) = pix[((i + ii) * w) + (j + jj)];
@@ -331,37 +340,29 @@ int main(int argc, char** argv) {
       }
       // Image encoding
       ctb = RGBtoYCBCR(b);
-      BlockSplitter(ctb, &yBlock, &cbBlock, &crBlock);
-      yBlock = DCTransform(yBlock);
-      cbBlock = DCTransform(cbBlock);
-      crBlock = DCTransform(crBlock);
-      Quantization(&yBlock);
-      Quantization(&cbBlock);
-      Quantization(&crBlock);
-      yEBlock = RLEncoding(yBlock);
-      cbEBlock = RLEncoding(cbBlock);
-      crEBlock = RLEncoding(crBlock);
+      BlockSplitter(ctb, &channels[0], &channels[1], &channels[2]);
+      for (int c = 0; c < 3; c++) {
+        channels[c] = DCTransform(channels[c]);
+        Quantization(&channels[c]);
+        encodedCh[c] = RLEncoding(channels[c]);
+      }
       // Image decoding
-      yBlock = RLDecoding(yEBlock);
-      cbBlock = RLDecoding(cbEBlock);
-      crBlock = RLDecoding(crEBlock);
-      Unquantize(&yBlock);
-      Unquantize(&cbBlock);
-      Unquantize(&crBlock);
-      yBlock = IDCTransform(yBlock);
-      cbBlock = IDCTransform(cbBlock);
-      crBlock = IDCTransform(crBlock);
-      BlockJoiner(&ctb, yBlock, cbBlock, crBlock);
+      for (int c = 0; c < 3; c++) {
+        channels[c] = RLDecoding(encodedCh[c]);
+        Unquantize(&channels[c]);
+        channels[c] = IDCTransform(channels[c]);
+      }
+      BlockJoiner(&ctb, channels[0], channels[1], channels[2]);
       b = YCBCRtoRGB(ctb);
       for (int ii = 0; ii < 8; ii++) {
         for (int jj = 0; jj < 8; jj++) {
           auto& ip = pix[((i + ii) * w) + (j + jj)];
           auto& dp = dif[((i + ii) * w) + (j + jj)];
           const auto& bp = b.el(ii, jj);
-          dp.r = min(255, (int)pow(2, abs(ip.r - bp.r)));
-          dp.g = min(255, (int)pow(2, abs(ip.g - bp.g)));
-          dp.b = min(255, (int)pow(2, abs(ip.b - bp.b)));
-          dp.a = 255;
+          int diffR = pow(2, abs(ip.r - bp.r));
+          int diffG = pow(2, abs(ip.g - bp.g));
+          int diffB = pow(2, abs(ip.b - bp.b));
+          dp = RGBA8(min(255, diffR), min(255, diffG), min(255, diffB));
           ip = bp;
         }
       }
