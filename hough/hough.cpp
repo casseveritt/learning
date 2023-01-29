67bf87756d49ffacdf0d386d7126e59e2f4d70f1
Cass Everitt <cass@xyzw.us>
2023-01-30 00:17:24 +0100

    add test input image, and make it default
    output with no args:
    input image name=images/diamond.png, w=620, h=611, n=1

--- hough/hough.cpp
+++ hough/hough.cpp
@@ -12,16 +12,14 @@ using namespace std;
 int main(int argc, char** argv) {
   // Setting color space transform matricies
   string imgName = "images/";
-  imgName += (argc >= 2) ? argv[1] : "lines.png";
-
-  fmt::print("file name = {}\n", imgName);
-
-  return 0;
+  imgName += (argc >= 2) ? argv[1] : "diamond.png";
 
   int w, h, n;
   unsigned char* img;
   img = image_load(imgName.c_str(), &w, &h, &n);
 
-  image_store_png("out.png", w, h, 4, img);
+  fmt::print("input image name={}, w={}, h={}, n={}\n", imgName, w, h, n);
+
+  // image_store_png("out.png", w, h, 4, img);
   return 0;
 }
